961e5aad7454b3e043635d98b33a560cbf52326c
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-05-17 15:51:42 +0200

    sound effects

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ WHITE = (255,255,255)
 
 BORDER = pygame.Rect(WIDTH // 2  - 5, 0, 10, HEIGHT)
 
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
+
+
 HEALTH_FONT = pygame.font.SysFont('lexend', 40)
 WINNER_FONT = pygame.font.SysFont("lexend", 100)
 
@@ -125,16 +129,20 @@ def main():
                   if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                         bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height // 2 - 2, 10, 5)
                         yellow_bullets.append(bullet)
+                        BULLET_FIRE_SOUND.play()
 
                   if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                         bullet = pygame.Rect(red.x, red.y + red.height // 2 - 2, 10, 5)
                         red_bullets.append(bullet)
+                        BULLET_FIRE_SOUND.play()
 
             if event.type == RED_HIT:
                   red_health -= 1
+                  BULLET_HIT_SOUND.play()
 
             if event.type == YELLOW_HIT:
                   yellow_health -= 1
+                  BULLET_HIT_SOUND.play()
 
         winner_text = ""            
         if red_health <= 0:
